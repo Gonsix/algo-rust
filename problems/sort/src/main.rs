98a83d066cd97ecffdffee100e9cb726f61b9dff
Shingo Morimoto <shingo@wolfssl.com>
2024-03-25 17:35:28 +0900

    Add bubble sort and its unit test

--- problems/sort/src/main.rs
+++ problems/sort/src/main.rs
@@ -4,9 +4,18 @@ use sort::bubble_sort;
 use sort::selection_sort;
 use sort::quick_sort;
 fn main() {
+
+    let mut vec1 = vec![5, 3, 1, 90, 0];
+    println!("{:?}", vec1);
+
+    bubble_sort::sort(&mut vec1);
+    println!("{:?}", vec1);
+
+
     insertion_sort::sort();
     selection_sort::sort();
-    bubble_sort::sort();
+    // bubble_sort::sort();
     merge_sort::sort();
     quick_sort::sort();
+
 }
--- problems/sort/src/sort_algorithms/bubble_sort.rs
+++ problems/sort/src/sort_algorithms/bubble_sort.rs
@@ -1,3 +1,34 @@
-pub fn sort() {
-    println!("bubble_sort::sort !!");
+/// Sort an array into ascending order.
+pub fn sort<T: PartialOrd>(arr: &mut [T]) {
+    let mut n = arr.len();
+    if n == 0 {
+        return;
+    }
+    let mut swapped = true;
+
+    while swapped {
+        swapped = false;
+        for i in 1..n {
+            if arr[i-1] > arr[i] { // デカい値の要素を後ろへ持っていく
+                arr.swap(i, i-1);
+                swapped = true;
+            }
+        }
+        n -= 1
+    }
+
+}
+
+#[cfg(test)]
+
+mod tests {
+
+    use super::sort;
+
+    #[test]
+    fn test_sort_empty_array() {
+        let mut empty: Vec<i32> = vec![];
+        sort(&mut empty);
+        assert_eq!(empty, vec![]);
+    }
 }
