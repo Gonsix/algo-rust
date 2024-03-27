a895b09c0c713d1512ccfe40ec84aefd83d03fe3
Shingo Morimoto <shingo@wolfssl.com>
2024-03-27 14:20:25 +0900

    Add randomized float vector Generator for testing

--- problems/sort/src/utils/random.rs
+++ problems/sort/src/utils/random.rs
@@ -9,7 +9,13 @@ pub fn gen_random_usize(max_limit: usize) -> usize {
     return num;
 }
 
+/// Generate randomized Integer vector
 pub fn gen_random_vec_i32(size: usize, upper_limit: i32) -> Vec<i32> {
     let mut rng = rand::thread_rng(); // 乱数生成器を初期化
     (0..size).map(|_| rng.gen_range(0..upper_limit)).collect()
+}
+/// Generate randomized Float vector
+pub fn gen_random_vec_f64(size: usize, upper_limit: f64) -> Vec<f64> {
+    let mut rng = rand::thread_rng(); // 乱数生成器を初期化
+    (0..size).map(|_| rng.gen::<f64>() * upper_limit).collect()
 }
